7bd8e758b14ec79f3cc9b3636af2837e2fb20bbb
JaiminRaval <siliconvalley.jaimin@gmail.com>
2024-08-08 23:19:38 +0530

    viewController lifecycle code added

--- Weather/Controller/DetailVC.swift
+++ Weather/Controller/DetailVC.swift
@@ -9,10 +9,94 @@ import UIKit
 
 class DetailVC: UIViewController {
 
+    override func viewWillAppear(_ animated: Bool) {
+        debugPrint("\n-----viewWillAppear called")
+        print("")
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+//        apicall()
+        debugPrint("\n-----viewDidLoad called")
+        print("")
 
-        // Do any additional setup after loading the view.
+
+    }
+    
+    
+    override func viewIsAppearing(_ animated: Bool) {
+        debugPrint("\n-----viewIsAppearing called")
+        view.backgroundColor = .red
+
+        print("")
+
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        debugPrint("\n-----viewDidAppear called")
+        print("")
+
+
+    }
+
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        debugPrint("\n-----viewDidDisappear called")
+        print("")
+
+
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        debugPrint("-----viewWillDisappear called")
+        print("")
+
+    }
+    
+    
+    func apicall() {
+        
+        let url = "https://dummyjson.com/test"
+        
+        if let apiurl = URL(string: url) {
+            
+            let session = URLSession.shared
+            
+//            let dataTask = session.dataTask(with: URL(string: url)!) { data, response, error in
+//                
+//            }
+            
+            let dataTask = session.dataTask(with: apiurl) { data, response, error in
+                
+//                if let error = error {
+//                    debugPrint("Something went wrong:\(error)")
+//                    return
+//                }
+//                
+//                guard let responseData = data else { 
+//                    debugPrint("No Data received!")
+//                    return
+//                }
+                
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data!) as? [String: Int] {
+//                        print("response: \(response!)")
+                        debugPrint("\n------JSON:\(json)")
+                        
+                    } else {
+                        debugPrint("-----error in json serialization")
+                    }
+                } catch {
+                    debugPrint("error in parsing Data!:\(error)")
+
+                }
+            }
+            dataTask.resume()
+        } else {
+            debugPrint("Invalid URL")
+        }
     }
     
 
---
+++ Weather/Model/StudentModel.swift
@@ -0,0 +1,15 @@
+//
+//  StudentModel.swift
+//  Weather
+//
+//  Created by Jaimin Raval on 05/08/24.
+//
+
+import Foundation
+
+
+struct StudentModel {
+    let name: String
+    let course: String
+    let rollno: Int?
+}
